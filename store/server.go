19a9ffc73c884fb2a1089d84aa9bbb2a492743ae
dongxug <huang@pingcap.com>
2018-03-10 16:00:01 +0800

    backup

--- store/server.go
+++ store/server.go
@@ -3,10 +3,15 @@ package store
 import (
 	"io"
 	"net/http"
+
+	"github.com/juju/errors"
+	log "github.com/sirupsen/logrus"
 )
 
 type Server struct {
 	Addr string
+
+	store *Store
 }
 
 func NewServer(addr string) *Server {
@@ -15,15 +20,21 @@ func NewServer(addr string) *Server {
 	}
 }
 
+func (s *Server) OpenStore(storePath string) error {
+	store := new(Store)
+	if err := store.Open(storePath, false); err != nil {
+		return errors.Trace(err)
+	}
+	s.store = store
+	return nil
+}
+
 func (s *Server) handler(w http.ResponseWriter, req *http.Request) {
 	io.WriteString(w, "hello, world!\n")
 }
 
 func (s *Server) Serve() error {
+	log.Info("HTTP server starts serving...", s.Addr)
 	http.HandleFunc("/", s.handler)
 	return http.ListenAndServe(s.Addr, nil)
-}
-
-func (s *Server) addBlob(ID uint64, rdr io.Reader) error {
-	return nil
 }
